694bbc31881624cd922a1f4ef6300f0fac3c1a76
Abhilash M <148137833+Abhi0804-M@users.noreply.github.com>
2025-09-17 01:24:09 +0530

    app.py

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def handle_ai():
 
 if __name__ == "__main__":
     # Start main Flask app
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
